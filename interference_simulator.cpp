b7fc2598b529cd70f8aff4012ae9a9f0183f30b9
abdmoh123 <abdhawisa@gmail.com>
2023-03-26 19:09:17 +0100

    Modify Noisifier class to read input as vector

--- interference_simulator.cpp
+++ interference_simulator.cpp
@@ -3,38 +3,77 @@
 //
 
 #include <random>
+#include <algorithm>
 
 using namespace std;
 
 class Noisifier {
+    /* Class for applying noise to data induced by wireless transmission */
+
 private:
     double mean = 0.0;
     double std = 1.0;
 public:
-    double gen_gaussian(const double noise_amp) {
+    Noisifier(double mean, double std) {
+        /* Constructor for instantiating a Noisifer object */
+
+        set_mean(mean);
+        set_std(std);
+    }
+
+    vector<double> gen_gaussian(const double noise_amp, const int vector_size) {
         /* Generates random values based on a gaussian distribution and an inputted amplitude */
 
+        vector<double> random_vector;
+
+        // sets the mean and std of the gaussian distribution
         default_random_engine generator;
         normal_distribution<double> dist(mean, std);
-        return noise_amp * dist(generator);
+
+        // generates a vector with random based on the distribution above
+        for (int i = 0; i < vector_size; ++i) {
+            random_vector.push_back(noise_amp * dist(generator));
+        }
+
+        return random_vector;
     }
 
-    double add_gaussian(const double input_data, const double noise_amp) {
+    vector<double> add_gaussian(const vector<double> &input_data, const double noise_amp) {
         /* Applies gaussian noise to inputted data */
 
-        return input_data + gen_gaussian(noise_amp);
+        vector<double> noisy_data;
+
+        // generates a random gaussian noise vector
+        vector<double> random_noise = gen_gaussian(noise_amp, (int) input_data.size());
+        // applies the random noise to the inputted data
+        transform(input_data.begin(), input_data.end(), random_noise.begin(), noisy_data.begin(),plus<>());
+
+        return noisy_data;
     }
 
-    double add_periodic_gaussian(
-        const double input_data,
-        const double time,
+    vector<double> add_periodic_gaussian(
+        const vector<double> &input_data,
         const double noise_amp,
         const double periodic_freq
     ) {
         /* Applies gaussian noise that varies with time periodically to inputted data */
 
-        double sine_noise = sin(periodic_freq * time) * gen_gaussian(noise_amp);
-        return input_data + sine_noise;
+        vector<double> noisy_data;
+        vector<double> sine_noise;
+
+        // generates a random gaussian noise vector
+        vector<double> random_noise = gen_gaussian(noise_amp, (int) input_data.size());
+        // generates a sine vector
+        for (int i = 0; i < input_data.size(); ++i) {
+            sine_noise.push_back(sin(periodic_freq * i));
+        }
+
+        // modulates noise to be sine-like (noise periodically increases and decreases)
+        transform(sine_noise.begin(), sine_noise.end(), random_noise.begin(), sine_noise.begin(), multiplies<>());
+        // applies the periodic noise to the inputted data
+        transform(input_data.begin(), input_data.end(), sine_noise.begin(), noisy_data.begin(),plus<>());
+
+        return noisy_data;
     }
 
     /* Attribute setters and getters */
