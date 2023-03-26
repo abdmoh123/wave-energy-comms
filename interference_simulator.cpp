f1831ca1682900d7209688bbb95c6b6f0af16d5d
abdmoh123 <abdhawisa@gmail.com>
2023-03-26 23:29:45 +0100

    - Remove attributes mean and std - Add attribute distance_m - Modify class methods to work with new changes - Create calculate_distance_coefficient function

--- interference_simulator.cpp
+++ interference_simulator.cpp
@@ -11,17 +11,23 @@ class Noisifier {
     /* Class for applying noise to data induced by wireless transmission */
 
 private:
-    double mean = 0.0;
-    double std = 1.0;
+    double distance_m;  // distance between transmitter and receiver in metres
 public:
-    Noisifier(double mean, double std) {
+    Noisifier(double distance_in) {
         /* Constructor for instantiating a Noisifer object */
 
-        set_mean(mean);
-        set_std(std);
+        this->distance_m = distance_in;
     }
 
-    vector<double> gen_gaussian(const double noise_amp, const int vector_size) {
+    double calculate_distance_coefficient() {
+        /* Calculates how much noise is generated based on distance between the 2 transceivers */
+
+        return get_distance();  // temporarily just returns the distance
+    }
+
+    vector<double> gen_gaussian_noise(
+        const double noise_amp, const int vector_size, const double mean, const double std
+    ) {
         /* Generates random values based on a gaussian distribution and an inputted amplitude */
 
         vector<double> random_vector;
@@ -31,29 +37,37 @@ public:
         normal_distribution<double> dist(mean, std);
 
         // generates a vector with random based on the distribution above
-        for (int i = 0; i < vector_size; ++i) {
-            random_vector.push_back(noise_amp * dist(generator));
+        for (int _ = 0; _ < vector_size; ++_) {
+            // distance between the 2 transceivers affects the amplitude of the noise
+            random_vector.push_back(noise_amp * calculate_distance_coefficient() * dist(generator));
         }
 
         return random_vector;
     }
 
-    vector<double> add_gaussian(const vector<double> &input_data, const double noise_amp) {
+    vector<double> add_gaussian_noise(
+        const vector<double> &input_data,
+        const double noise_amp,
+        const double mean,
+        const double std
+    ) {
         /* Applies gaussian noise to inputted data */
 
         vector<double> noisy_data;
 
         // generates a random gaussian noise vector
-        vector<double> random_noise = gen_gaussian(noise_amp, (int) input_data.size());
+        vector<double> random_noise = gen_gaussian_noise(noise_amp, (int) input_data.size(), mean, std);
         // applies the random noise to the inputted data
         transform(input_data.begin(), input_data.end(), random_noise.begin(), noisy_data.begin(),plus<>());
 
         return noisy_data;
     }
 
-    vector<double> add_periodic_gaussian(
+    vector<double> add_periodic_gaussian_noise(
         const vector<double> &input_data,
         const double noise_amp,
+        const double mean,
+        const double std,
         const double periodic_freq
     ) {
         /* Applies gaussian noise that varies with time periodically to inputted data */
@@ -62,7 +76,7 @@ public:
         vector<double> sine_noise;
 
         // generates a random gaussian noise vector
-        vector<double> random_noise = gen_gaussian(noise_amp, (int) input_data.size());
+        vector<double> random_noise = gen_gaussian_noise(noise_amp, (int) input_data.size(), mean, std);
         // generates a sine vector
         for (int i = 0; i < input_data.size(); ++i) {
             sine_noise.push_back(sin(periodic_freq * i));
@@ -77,16 +91,10 @@ public:
     }
 
     /* Attribute setters and getters */
-    void set_mean(const double mean_in) {
-        this->mean = mean_in;
+    void set_distance(const double distance_in) {
+        this->distance_m = distance_in;
     }
-    void set_std(const double std_in) {
-        this->std = std_in;
-    }
-    double get_mean() {
-        return mean;
-    }
-    double get_std() {
-        return std;
+    double get_distance() {
+        return distance_m;
     }
 };
