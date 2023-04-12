41513a9f749960a1516e01a8604d12c91f65df9f
abdmoh123 <abdhawisa@gmail.com>
2023-04-13 00:12:51 +0100

    - Remove add_white_gaussian_noise function - Fix attenuate function by using db2mag - Add gain_tx and gain_rx parameters to attenuate functions - Add lora_frequency signal property variable to main.m - Remove noisy_signal (not attenuated) from main.m - Use lora_frequency instead of sample_frequency for attenuation and fspl calculation - Remove noisy_signal plots - General code cleanup (comments, indentation etc.)

--- InterferenceSimulator.m
+++ InterferenceSimulator.m
@@ -16,41 +16,34 @@ classdef InterferenceSimulator
             %% Calculates the Free Space Path Loss for a 2 transceiver system
 
             % takes into account the gains of the transceiver antennas
-            output_arg = fspl(obj.distance, physconst('LightSpeed')/frequency) - gain_tx - gain_rx;
+            output_arg = fspl( ...
+                obj.distance, physconst('LightSpeed') / frequency ...
+            ) - gain_tx - gain_rx;
         end
 
-        function output_arg = attenuate(obj, input_signal, frequency)
+        function output_arg = attenuate(obj, input_signal, frequency, gain_tx, gain_rx)
             %% Attenuates the signal using the distance between the transceivers
 
+            fspl = calc_fspl(obj, frequency, gain_tx, gain_rx);
+            % calculates the transmit and receive power values using the fspl
             transmit_power = 10 * log10(rms(input_signal) .^ 2);
-            fspl = calc_fspl(obj, frequency, 0, 0);
             receive_power = transmit_power / fspl;
 
-            output_arg = input_signal * (receive_power / transmit_power);
+            power_diff = receive_power - transmit_power; % should always be < 0
+            output_arg = input_signal * db2mag(power_diff); % converts to linear scale
         end
 
-        function output_arg = attenuate_with_noise(obj, input_signal, frequency, signal_noise_ratio)
-            %% Attenuates and applies noise to the LoRa signal (noise does not get attenuated)
+        function output_arg = attenuate_with_noise( ...
+            obj, input_signal, frequency, gain_tx, gain_rx, signal_noise_ratio ...
+        )
+            %% Attenuates then applies noise to the LoRa signal (noise does not get attenuated)
 
-            % generates noise using only a signal to noise ratio
-            noisy_signal = add_white_noise(obj, input_signal, signal_noise_ratio);
-            % separates noise from the input signal
-            noise = noisy_signal - input_signal;
+            % generates noise using only a signal to noise ratio based on
+            % the original modulated signal (not attenuated)
+            noisy_signal = awgn(input_signal, signal_noise_ratio, 'measured');
+            noise = noisy_signal - input_signal; % separates noise from the input signal
 
-            output_arg = attenuate(obj, input_signal, frequency) + noise;
-        end
-
-        function output_arg = gen_white_noise(~, noise_power, load_impedance, num_rows, num_cols)
-            %% Generates white gaussian noise (wrapper function)
-            
-            % noise is generated using a seed of 1 (for reproducible results)
-            output_arg = wgn(num_rows, num_cols, power=noise_power, imp=load_impedance, seed=1);
-        end
-
-        function output_arg = add_white_noise(~, input_signal, signal_noise_ratio)
-            %% Applies white gaussian noise
-
-            output_arg = awgn(input_signal, signal_noise_ratio, 'measured');
+            output_arg = attenuate(obj, input_signal, frequency, gain_tx, gain_rx) + noise;
         end
     end
 end
--- main.m
+++ main.m
@@ -9,6 +9,7 @@ sample_frequency = 10e6; % Sampling frequency
 carrier_frequency_offset = 915e6 - 921.5e6; % Frequency offset
 transmit_power = 14; % Transmission power (in dBm)
 signal_noise_ratio = -5; % Signal Noise Ratio
+lora_frequency = 868e6; % 868MHz LoRa band is used (license exempt)
 
 % Message/payload being transmitted
 message = "emf:10.0;acc:3;.;";
@@ -31,18 +32,16 @@ disp(['Transmit Power (Pt) = ' num2str(Pt) ' dBm'])
 %% Noise and attenuation
 interf_sim = InterferenceSimulator(3e3); % 3km distance
 
-% modulated signal with added noise
-noisy_signal = interf_sim.add_white_noise(modulated_signal, signal_noise_ratio);
 % modulated signal with attenuation as well as added noise
 atten_noisy_signal = interf_sim.attenuate_with_noise( ...
-    modulated_signal, sample_frequency, signal_noise_ratio ...
+    modulated_signal, lora_frequency, 0, 0, signal_noise_ratio ...
 );
 
 % calcuates and prints the Free Space Path Loss
-fspl = interf_sim.calc_fspl(sample_frequency, 0, 0);
+fspl = interf_sim.calc_fspl(lora_frequency, 0, 0);
 disp(['Free Space Path Loss = ' num2str(fspl) ' dB'])
 
-% calculates and prints the transmitted power (should equal the predefined value)
+% calculates and prints the transmitted power
 Pr = 10 * log10(rms(atten_noisy_signal) .^ 2);
 disp(['Receive Power (Pr) = ' num2str(Pr) ' dBm'])
 
@@ -68,20 +67,11 @@ title("Frequency spectrum clean modulated signal");
 figure(2)
 obw(modulated_signal, sample_frequency);
 
-%% Noisy signal plots
-% plots frequency spectrum of the noisy modulated signal
-figure(3)
-spectrogram(noisy_signal, 500, 0, 500, sample_frequency, 'yaxis', 'centered')
-title("Frequency spectrum noisy modulated signal")
-% plots the 99% occupied bandwidth of the noisy modulated signal
-figure(4)
-obw(noisy_signal, sample_frequency);
-
 %% Attenuated noisy signal plots
 % plots frequency spectrum of the attenuated modulated signal (with added noise)
-figure(5)
+figure(3)
 spectrogram(atten_noisy_signal, 500, 0, 500, sample_frequency, 'yaxis', 'centered')
 title("Frequency spectrum of attenuated noisy modulated signal")
 % plots the 99% occupied bandwidth of the attenuated noisy modulated signal
-figure(6)
+figure(4)
 obw(atten_noisy_signal, sample_frequency);
