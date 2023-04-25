428331fc1bb44410c8e63d72804084ed524376c9
abdmoh123 <abdhawisa@gmail.com>
2023-04-25 17:02:22 +0100

    Add ethernet code to receiver

--- arduino_sketches/lora_receiver/lora_receiver.ino
+++ arduino_sketches/lora_receiver/lora_receiver.ino
@@ -1,10 +1,23 @@
 #include <SPI.h>
 #include <LoRa.h>
+#include <Ethernet.h>
+
+// mac address must be unique
+byte mac_address[] = {
+	0xAB, 0xFE, 0xFF, 0xFB, 0xEB, 0xCD
+};
+// might need to be changed
+IPAddress ip(192, 168, 0, 78);
+
+// Ethernet server is initialised
+// port 80 is default for HTTP
+EthernetServer server(80);
 
 void setup() {
+  Ethernet.init(5); // Ethernet shield
+  
   Serial.begin(9600);
   while (!Serial);
-
   Serial.println("LoRa Receiver");
 
   // halts program if lora failed to start
@@ -12,6 +25,25 @@ void setup() {
     Serial.println("Starting LoRa failed!");
     while (1);
   }
+  
+  // starts the Ethernet connection and server
+  Ethernet.begin(mac_address, ip);
+  
+  // check for Ethernet hardware present
+  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+    while (true) {
+      delay(1); // do nothing, no point running without Ethernet hardware
+    }
+  }
+  if (Ethernet.linkStatus() == LinkOFF) {
+    Serial.println("Ethernet cable is not connected.");
+  }
+
+  // start the server
+  server.begin();
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
 }
 
 void loop() {
@@ -30,5 +62,55 @@ void loop() {
     // print RSSI of packet
     Serial.print("' with RSSI ");
     Serial.println(LoRa.packetRssi());
+  }
+  
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+          // output the value of each analog input pin
+          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
+            int sensorReading = analogRead(analogChannel);
+            client.print("analog input ");
+            client.print(analogChannel);
+            client.print(" is ");
+            client.print(sensorReading);
+            client.println("<br />");
+          }
+          client.println("</html>");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
   }
 }
